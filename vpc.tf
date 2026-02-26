8433ac34b925189139007a62cfef1241d24f4550
gunasekhar <test@ow.cm>
2026-02-26 16:07:49 +0000

    Adding vpc.tf and tfnetout

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -6,3 +12,14 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
